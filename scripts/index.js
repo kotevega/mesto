b01dab01f54405340c66cb6de678f100ba698c93
Konstantin Logunov <keniglokon@yandex.ru>
2023-02-19 23:13:09 +0200

    fix:исправления ошибок

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+let editPtofileButton = document.querySelector("#profile-edit-button");
+let editPopup = document.querySelector("#popup");
+let closePopup = document.querySelector("#close-popup");
+let profileNameInput = document.querySelector("#profile-name");
+let profileJobInput = document.querySelector("#profile-occupation");
+let popupNameInput = document.querySelector("#popup-user-name");
+let popupJobInput = document.querySelector("#popup-user-job");
+let formElement = document.querySelector(".popup__container");
+
+editPtofileButton.addEventListener("click", function () {
+  editPopup.classList.add("popup_opened");
+  popupNameInput.value = profileNameInput.innerHTML;
+  popupJobInput.value = profileJobInput.innerHTML;
+  profileNameInput.textContent = popupNameInput.value;
+  profileJobInput.textContent = popupJobInput.value;
+});
+
+closePopup.addEventListener("click", closePopupUser);
+
+function closePopupUser() {
+  editPopup.classList.remove("popup_opened");
+}
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileNameInput.textContent = popupNameInput.value;
+  profileJobInput.textContent = popupJobInput.value;
+  closePopupUser();
+}
+formElement.addEventListener("submit", handleFormSubmit);
